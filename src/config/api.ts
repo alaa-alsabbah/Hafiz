1eda2204a15a8488d79c1af5f972403e7e4aa007
Your Name <alaa.alsabbahh@gmail.com>
2026-02-26 01:34:46 +0300

    add api after hte url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,11 @@
 // API configuration
-// Defaults match: {{base_url}} = http://34.247.136.158/api, {{version}} = v1
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://34.247.136.158/api'
+// Defaults match: {{base_url}} = https://api.hafatha.com/api, {{version}} = v1
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.hafatha.com/api'
 export const API_VERSION = import.meta.env.VITE_API_VERSION || 'v1'
+
+
+
+// // API configuration
+// // Defaults match: {{base_url}} = https://api.hafatha.com/api, {{version}} = v1
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.hafatha.com/api'
+// export const API_VERSION = import.meta.env.VITE_API_VERSION || 'v1'
