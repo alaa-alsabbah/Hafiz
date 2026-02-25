add9eedfcb31a6706ebd00061a01b1ac0bd24c7e
Your Name <alaa.alsabbahh@gmail.com>
2026-02-26 01:37:35 +0300

    change the url

--- src/config/api.ts
+++ src/config/api.ts
@@ -6,6 +6,7 @@ export const API_VERSION = import.meta.env.VITE_API_VERSION || 'v1'
 
 
 // // API configuration
-// // Defaults match: {{base_url}} = https://api.hafatha.com/api, {{version}} = v1
-// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.hafatha.com/api'
+// // Defaults match: {{base_url}} = http://34.247.136.158/api, {{version}} = v1
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://34.247.136.158/api'
 // export const API_VERSION = import.meta.env.VITE_API_VERSION || 'v1'
+
